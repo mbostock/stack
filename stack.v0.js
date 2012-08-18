eccf28aff889192a7df595f1bf6f590e92b946ce
Mike Bostock <mbostock@gmail.com>
2012-08-18 15:35:44 -0700

    Don't leap if the offset is tiny.

--- stack.v0.js
+++ stack.v0.js
@@ -62,7 +62,7 @@ var stack = (function() {
 
   // if scrolling up, jump to edge of previous slide
   function leap(yNew) {
-    if ((yActual == yFloor) && (yNew < yActual)) {
+    if (yOffset > 10 && (yActual == yFloor) && (yNew < yActual)) {
       event.deactivate.call(section[0][yFloor], yFloor);
       event.activate.call(section[0][--yFloor], yFloor);
       yActual -= .5 - yOffset / size / 2;
